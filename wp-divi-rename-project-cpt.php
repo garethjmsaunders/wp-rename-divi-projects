f9e955158f072d65b947c1827348fd7d1e22da6c
Gareth J M Saunders <gareth@garethjmsaunders.co.uk>
2026-02-14 22:48:17 +0000

    FORMAT (Licence) Update licence info
    Fix `GPL3` to `GPLv3` to make it clearer.
    Rename `license.txt` to `LICENSE` as this is the GitHub convention.

--- wp-divi-rename-project-cpt.php
+++ wp-divi-rename-project-cpt.php
@@ -10,7 +10,7 @@
  * Domain Path:         /languages/
  * Requires at least:   6.0
  * Tested up to:        6.9.1
- * License:             GPL3
+ * License:             GPLv3
  * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
  */
 
