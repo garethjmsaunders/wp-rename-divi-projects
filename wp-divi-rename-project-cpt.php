90f75d6a2eb209517553e96bf9edd7bd157ef8b5
Gareth J M Saunders <gareth@garethjmsaunders.co.uk>
2026-02-14 22:11:27 +0000

    FIX (Plugin) Fix Requires at least version to match Divi 5
    To claim Divi 5 support, this plugin’s WordPress minimum should align with Divi 5’s minimum which is v6.0.
    
    Divi 4 still claims to support WordPress 5.3+.

--- wp-divi-rename-project-cpt.php
+++ wp-divi-rename-project-cpt.php
@@ -8,7 +8,7 @@
  * Author URI:          https://digitalshed45.co.uk
  * Text domain:         wp-divi-rename-project-cpt
  * Domain Path:         /languages/
- * Requires at least:   5.3
+ * Requires at least:   6.0
  * Tested up to:        6.9.1
  * License:             GPL3
  * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
